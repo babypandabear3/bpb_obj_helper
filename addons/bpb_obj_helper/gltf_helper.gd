77b7e51eadd341d66a780ca7dd82425a7b505abe
babypandabear3 <manusiatidakbiasa@gmail.com>
2021-10-13 16:09:42 +0800

    - add collision

--- addons/bpb_obj_helper/gltf_helper.gd
+++ addons/bpb_obj_helper/gltf_helper.gd
@@ -6,13 +6,14 @@ class_name BPB_Gltf_helper
 export (bool) var refresh = false setget refresh_set
 export (String, FILE, "*.glb, *.gltf") var obj_file = "" setget obj_file_set
 export (bool) var grid_tex = false setget grid_tex_set
+export (bool) var collision = true setget collision_tex_set
 var mesh_obj
+
+var staticbodies = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
 
 func obj_file_set(file_path):
 	if file_path == null:
@@ -39,7 +40,9 @@ func update_mesh():
 			apply_grid_tex(true)
 		else:
 			apply_grid_tex(false)
-	print(name, " : meshes and collision updated")
+			
+		update_collision()
+	print("BPB_Gltf_helper : meshes and collision updated")
 			
 func apply_grid_tex(par):
 	if par:
@@ -47,7 +50,7 @@ func apply_grid_tex(par):
 		recursive_apply_mat(self, mat)
 	else:
 		recursive_apply_mat(self, null)
-	print(name, " : material updated")
+	print("BPB_Gltf_helper : material updated")
 		
 func refresh_set(value):
 	if value:
@@ -64,3 +67,25 @@ func recursive_apply_mat(parent, mat):
 	if parent is MeshInstance:
 		parent.material_override = mat
 			
+func collision_tex_set(par):
+	collision = par
+	update_collision()
+		
+func update_collision():
+	staticbodies.clear()
+	recursive_collision(self)
+	if not collision:
+		for i_id in staticbodies:
+			instance_from_id(i_id).free()
+	
+func recursive_collision(parent):
+	for o in parent.get_children():
+		recursive_collision(o)
+	if collision:
+		if parent is MeshInstance:
+			parent.create_trimesh_collision()
+	else:
+		if parent is StaticBody:
+			staticbodies.append(parent.get_instance_id())
+		
+	
